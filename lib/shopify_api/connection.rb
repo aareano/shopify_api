fb291c52a58fff9bd8d1d8c09262e28adb470158
Aaron Bowen <aaron.bwn@gmail.com>
2016-10-22 19:09:25 -0400

    add retry support for calls to Shopify's API

--- lib/shopify_api/connection.rb
+++ lib/shopify_api/connection.rb
@@ -2,6 +2,9 @@ module ShopifyAPI
   class Connection < ActiveResource::Connection
     attr_reader :response
 
+    RETRY_AFTER = 2 # seconds
+    RETRY_TIMES = 5 # times
+
     module ResponseCapture
       def handle_response(response)
         @response = super
@@ -12,8 +15,15 @@ module ShopifyAPI
 
     module RequestNotification
       def request(method, path, *arguments)
-        super.tap do |response|
-          notify_about_request(response, arguments)
+        retry_times = RETRY_TIMES
+        begin
+          super.tap do |response|
+            notify_about_request(response, arguments)
+          end
+        rescue ActiveResource::ClientError => e
+          raise unless e.response.code.to_i == 429 && !(retry_times -= 1).zero?
+          sleep((e.response['Retry-After'].presence || RETRY_AFTER).to_i)
+          retry
         end
       rescue => e
         notify_about_request(e.response, arguments) if e.respond_to?(:response)
